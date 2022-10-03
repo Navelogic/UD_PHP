097e0da4ee77ab2e9858f5389809cd036c506d85
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-03 19:58:37 -0300

    Primeira aula (aquecimento)

---
+++ code.js
@@ -0,0 +1,12 @@
+let primeiroNum = 4;
+let segundoNum = 4;
+
+let somaNums = (primeiroNum + segundoNum);
+console.log(`A soma de ${primeiroNum} + ${segundoNum} é ${somaNums}. ${somaNums} x ${primeiroNum} = ${somaNums * primeiroNum}.`);
+
+
+let terceiroNum = 9;
+let quartoNum = 5;
+let quintoNum = 9;
+let media = (terceiroNum + quartoNum + quartoNum)/3;
+console.log(`A média é ${media.toFixed(2)}`);

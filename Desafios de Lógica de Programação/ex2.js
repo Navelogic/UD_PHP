f6dedd71007a196a9afa7444bc22337700ae4388
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-04 15:38:32 -0300

    Letra a, ex2

---
+++ Desafios de Lógica de Programação/ex2.js
@@ -0,0 +1,8 @@
+// Analise os algoritmos abaixo e diga o que será impresso na tela ao serem executados: 
+let a = 10;
+let b = 20;
+console.log(`O valor de b é ${b}`);
+b = 5;
+console.log(`Algo mudou! o valor de a é ${a} e o valor de b é ${b}`);
+
+

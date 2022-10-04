617bc5fe339956490ed963e8d8d6fe9dfcd5efdc
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-04 15:42:16 -0300

    letra c ex2

--- Desafios de Lógica de Programação/ex2.js
+++ Desafios de Lógica de Programação/ex2.js
@@ -1,10 +1,13 @@
 // Analise os algoritmos abaixo e diga o que será impresso na tela ao serem executados: 
+
+//a)
 let a = 10;
 let b = 20;
 console.log(`O valor de b é ${b}`);
 b = 5;
 console.log(`Algo mudou! o valor de a é ${a} e o valor de b é ${b}`);
 
+//b)
 a = 30;
 b = 20;
 let c = a + b;
@@ -14,4 +17,10 @@ console.log(b, c);
 c = a = b;
 console.log(a, b, c);
 
-
+//c)
+a = 10;
+b = 20;
+c = a;
+b = c;
+a = b;
+console.log(a, b, c);

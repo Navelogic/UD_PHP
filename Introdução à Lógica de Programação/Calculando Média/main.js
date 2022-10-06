30e652d0b68cfad70895dd00a7b64614fd505f69
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-06 20:06:42 -0300

    melhorias gerais

--- Introdução à Lógica de Programação/Calculando Média/main.js
+++ Introdução à Lógica de Programação/Calculando Média/main.js
@@ -7,16 +7,14 @@ mediaQuatro = parseFloat(prompt(`Qual foi a nota de ${aluno} no QUARTO semestre?
 
 soma = (mediaUm + mediaDois + mediaTres + mediaQuatro);
 mediaTotal = soma/4;
+situacao = "Apovado"
 
 if(mediaTotal >= 9){
   document.write(`${aluno} está APROVADO! Sua média é ${mediaTotal}! Você tirou um A`);
-  situacao = "Aprovado";
 } else if(mediaTotal >= 7){
   document.write(`${aluno} está APROVADO! Sua média é ${mediaTotal}! Você tirou um B`);
-  situacao = "Aprovado";
 } else if(mediaTotal >= 6){
   document.write(`${aluno} está APROVADO! Sua média é ${mediaTotal}! Você tirou um C`);
-  situacao = "Aprovado";
 } else if(mediaTotal >= 4){
   document.write(`${aluno} está REPROVADO! Sua média é ${mediaTotal}! Você tirou um D`);
   situacao = "Reprovado";
@@ -29,4 +27,4 @@ if(situacao == "Reprovado"){
   mediaEmergente = parseFloat(prompt(`${aluno} foi reprovado, por favor passe mais uma nota.`));
   soma = (mediaUm + mediaDois + mediaTres + mediaQuatro + mediaEmergente);
   mediaTotal = soma/5;
-}
+};

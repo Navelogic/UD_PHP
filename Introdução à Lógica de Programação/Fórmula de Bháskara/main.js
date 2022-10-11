950046d1a2777f7e3c1ef47e07be1ff1fa6c6e9f
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-10 21:16:37 -0300

    Finalização

--- Introdução à Lógica de Programação/Fórmula de Bháskara/main.js
+++
@@ -1,3 +0,0 @@
-let a, b, c, delta, potenc, mult, x1, x2, mult1, mult2, divi, raiz, real;
-
-Math.sqrt
---
+++ Introdução à Lógica de Programação/for/main.js
@@ -0,0 +1,21 @@
+let fatorial = 20;
+let resultado = fatorial;
+for (let i = 1; i < fatorial; i++) {
+    resultado *= i;
+}
+console.log(resultado);
+
+
+
+for(let i = 0; i < 500; i++){
+  let resultado = 1 + i;
+  console.log(resultado);
+};
+
+
+
+for(let i = 0; i < 500; i++){
+  if(i%2 === 0){
+    console.log(i)
+  }
+};

4c4cf1e5f2b297afbdfc57fae7f2f99b36dd2c55
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-11 19:22:20 -0300

    Segue

---
+++ Introdução à Lógica de Programação/for, do while e while/main.js
@@ -0,0 +1,12 @@
+for(let i = 0; i < 500; i++){
+  let resultado = 1 + i;
+  console.log(resultado);
+};
+
+let teste = true;
+do{
+  console.log("Teste");
+} while(teste);
+
+
+console.log(Infinity * 2);

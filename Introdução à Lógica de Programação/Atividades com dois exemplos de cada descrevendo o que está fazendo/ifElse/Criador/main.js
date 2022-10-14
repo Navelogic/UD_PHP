bb84bd7b2f1fd0a9dd0990bfe6326028002454fb
Arian Weslley <weslleysantoszbd@gmail.com>
2022-10-14 19:00:32 -0300

    Mudança de Organização

---
+++ Introdução à Lógica de Programação/Atividades com dois exemplos de cada descrevendo o que está fazendo/ifElse/Criador/main.js
@@ -0,0 +1,12 @@
+// Testando nome dado pelo USER
+
+let nome = prompt('Qual seu nome?');
+
+if(nome == 'Arian'){
+  document.write(`Olá mestre ${nome}`);
+} else {
+  document.write(`Você não é meu criador ${nome}!`);
+};
+
+
+
